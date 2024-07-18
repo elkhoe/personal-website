60af7000c6495004e3bbdbf4b98f70754dca231e
elkhoe <ekhoe.bg@gmail.com>
2024-07-17 22:42:47 -0700

    add vercel packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -6,4 +7,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
